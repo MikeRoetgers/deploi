0b61f3151fb30b79bae7f0b702a2974b33bb46d0
Mike Roetgers <mike@roetgers.org>
2017-07-28 17:11:22 +0200

    remove license template

--- cmd/client/cmd/job.go
+++ cmd/client/cmd/job.go
@@ -1,16 +1,3 @@
-// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import "github.com/spf13/cobra"
@@ -31,4 +18,6 @@ var jobListCmd = &cobra.Command{
 func init() {
 	RootCmd.AddCommand(jobCmd)
 	jobCmd.AddCommand(jobListCmd)
+
+	jobListCmd.Flags().BoolP("pending", "p", true, "Show pending jobs vs. done jobs")
 }
